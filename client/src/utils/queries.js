2a9c249f7709e35dac88bf80c0128c36c62721fd
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-08-02 15:21:50 -0400

    Queries and Mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+{
+  me {
+    _id
+    username
+    email
+    savedBooks {
+      _id
+      bookId
+      title
+      authors
+      description
+      image
+      link
+    }
+  }
+}`;
