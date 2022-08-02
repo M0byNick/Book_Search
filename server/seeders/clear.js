28ca797592c6b0d7e7f1e7d8e22857e963feedc5
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-08-02 14:54:55 -0400

    Saved books page

---
+++ server/seeders/clear.js
@@ -0,0 +1,9 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+const { Book, User } = require('../models');
+
+db.once('open', async () => {
+    await User.deleteMany({});
+}
+)
