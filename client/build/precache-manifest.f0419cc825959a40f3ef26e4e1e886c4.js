d686e002d9d4084d6baa7edc09b258bb04248433
Nicholas Morihisa <nicholasmorihisa@nicholaacstudio.mynetworksettings.com>
2022-08-02 15:53:18 -0400

    Fixed styling

---
+++ client/build/precache-manifest.f0419cc825959a40f3ef26e4e1e886c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5076cbf0ec9965d9b757d09b8d77137c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a8eff757a298fda3c4",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "4c30791f8e5ad83a2d8f",
+    "url": "/static/css/main.7b6d6cb0.chunk.css"
+  },
+  {
+    "revision": "55a8eff757a298fda3c4",
+    "url": "/static/js/2.e874670b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.e874670b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c30791f8e5ad83a2d8f",
+    "url": "/static/js/main.cd05ecb1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.cd05ecb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
